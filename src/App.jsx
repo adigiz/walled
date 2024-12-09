9c82da364b96f106ef230982edb753cc259519d8
Adityo <adigiz@yahoo.com>
2024-12-09 13:40:23 +0700

    feat: add state to change tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
+import { useState } from "react";
 import logo from "./assets/logo.png";
 import NavItems from "./components/NavItems";
 
 function App() {
-
+  const [activeTab, setActiveTab] = useState("Dashboard");
   const menu = [
     {
       title: "Dashboard",
-      link: "#"
+      link: "#",
     },
     {
       title: "Transfer",
-      link: "#"
+      link: "#",
     },
     {
       title: "Topup",
-      link: "#"
+      link: "#",
     },
     {
       title: "Signout",
-      link: "#"
+      link: "#",
     },
-  ]
+  ];
+
   return (
     <>
       <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
         <img src={logo} />
         <div className="flex gap-x-8">
-          <NavItems menu={menu} />
+          <NavItems
+            menu={menu}
+            activeTab={activeTab}
+            handleClick={setActiveTab}
+          />
         </div>
       </nav>
     </>
