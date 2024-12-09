854455f62e60fc05023e305e4d4180c7a4de148e
Adityo <adigiz@yahoo.com>
2024-12-09 11:46:06 +0700

    feat: add new nav item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import "./App.css";
+import logo from "./assets/logo.png";
+import NavItems from "./components/NavItems";
 
 function App() {
+
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#"
+    },
+    {
+      title: "Transfer",
+      link: "#"
+    },
+    {
+      title: "Topup",
+      link: "#"
+    },
+    {
+      title: "Signout",
+      link: "#"
+    },
+  ]
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
     </>
   );
 }
