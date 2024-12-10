bdbdd862480d08b018cc712f1f226f0f908279ee
Damar Buana Murti <damarbuanamurti@gmail.com>
2024-12-10 20:35:12 +0700

    feat: add dark mode toggle

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,15 +1,31 @@
 import { NavLink } from "react-router";
+import { IoSunnyOutline, IoMoonOutline } from "react-icons/io5";
+import useTheme from "../context/ThemeContext";
 
 function NavItems({ menu }) {
+  const { themeMode, darkTheme, lightTheme } = useTheme();
+
+  const handleTheme = () => {
+    if (themeMode === "dark") {
+      lightTheme();
+    } else {
+      darkTheme();
+    }
+  };
+
+  console.log(themeMode);
+
   return (
-    <ul className="flex gap-x-8 text-black">
+    <ul className="flex gap-x-8">
       {menu.map((item) => {
         return (
           <NavLink
             key={item.title}
             to={item.link}
             className={({ isActive }) =>
-              isActive ? "text-[#19918F] font-bold" : "text-black"
+              isActive
+                ? "text-[#19918F] font-bold"
+                : "text-black dark:text-white"
             }
           >
             {item.title}
@@ -18,11 +34,18 @@ function NavItems({ menu }) {
       })}
       <NavLink
         to="/"
-        className="text-black"
+        className="text-black dark:text-white"
         onClick={() => localStorage.removeItem("login")}
       >
         Sign Out
       </NavLink>
+      <button onClick={() => handleTheme()}>
+        {themeMode === "dark" ? (
+          <IoSunnyOutline className="text-white" />
+        ) : (
+          <IoMoonOutline />
+        )}
+      </button>
     </ul>
   );
 }
